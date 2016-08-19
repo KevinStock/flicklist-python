f6dae3d3cc3bf9b80357c6c5ab686e3c4818715e
Kevin Stock <kevin@kevinstock.net>
2016-08-18 22:45:36 -0500

    studio3 complete

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
         # TODO: make a list with at least 5 movie titles
+        movie_list = ['Suicide Squad', 'Batman v Superman', 'Mad Max', 'Star Trek Beyond', 'The Legend of Tarzan', 'The Nice Guy', 'Captain America: Civil War']
 
         # TODO: randomly choose one of the movies, and return it
+        randnum = random.randint(0, len(movie_list) - 1)
 
-        return "The Big Lebowski"
+        return movie_list[randnum]
 
     def get(self):
         movie = self.getRandomMovie()
+        second_movie = self.getRandomMovie()
 
         # build the response string
         response = "<h1>Movie of the Day</h1>"
@@ -19,6 +23,9 @@ class Index(webapp2.RequestHandler):
 
         # TODO: pick a different random movie, and display it under
         # the heading "<h1>Tommorrow's Movie</h1>"
+        response += "<h1>Tomorrow's Movie</h1>"
+        response += "<p>" + second_movie + "</p>"
+
 
         self.response.write(response)
 
