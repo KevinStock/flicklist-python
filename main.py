134939fbea5ccf838ed3ae39113c414ac42b5cf5
Kevin Stock <kevin@kevinstock.net>
2016-08-18 22:49:49 -0500

    walkthrough3 complete in class

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import webapp2
 
 class Index(webapp2.RequestHandler):
+	def get_random_movie(self):
+		# TODO: Make acctually random
+		return 'Die Hard'
+
     def get(self):
-        self.response.write('Hello, Summer of Code!')
+        response = '<html><body>'
+        response += '<strong>Random Movie:</strong>'
+        response += self.get_random_movie()
+        response += '</body></html>'
+
+        self.response.write(response)
 
 app = webapp2.WSGIApplication([
     ('/', Index)
