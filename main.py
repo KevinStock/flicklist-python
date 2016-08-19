53303a57f65d74b632c03c01916339782374de00
Kevin Stock <kevin@kevinstock.net>
2016-08-18 22:51:42 -0500

    walkthrough4 complete in class

--- main.py
+++ main.py
@@ -1,31 +1,66 @@
 import webapp2
-import random
+#import random
 
 class Index(webapp2.RequestHandler):
 
-    def getRandomMovie(self):
+	html_head = """
+		<!doctype html>
+		<html>
+		<head>
+			<title>FlickListly</title>
+		</head>
+		<body>
 
-        # list of movies to select from
-        movies = ["The Big Lebowski", "Blue Velvet", "Toy Story", "Star Wars", "Amelie"]
+	"""
 
-        # randomly choose one of the movies
-        randomIdx = random.randrange(len(movies))
+	html_tail = """
+			</body>
+		</html>
+	"""
 
-        return movies[randomIdx]
+#
+#     def getRandomMovie(self):
+#
+#         # list of movies to select from
+#         movies = ["The Big Lebowski", "Blue Velvet", "Toy Story", "Star Wars", "Amelie"]
+#
+#         # randomly choose one of the movies
+#         randomIdx = random.randrange(len(movies))
+#
+#         return movies[randomIdx]
+#
+#     def get(self):
+#         # add Movie of the Day to the response string
+#         movie = self.getRandomMovie()
+#         response = "<h1>Movie of the Day</h1>"
+#         response += "<p>" + movie + "</p>"
+#
+#         # add Tomorrow's Movie to the response string
+#         tomorrow_movie = self.getRandomMovie()
+#         response += "<h1>Tomorrow's Movie</h1>"
+#         response += "<p>" + tomorrow_movie + "</p>"
+#
+#         self.response.write(response)
 
-    def get(self):
-        # add Movie of the Day to the response string
-        movie = self.getRandomMovie()
-        response = "<h1>Movie of the Day</h1>"
-        response += "<p>" + movie + "</p>"
+	def get(self):
+		html_form = """
+			<form id="add_form" action="/addmovie" method="POST">
+				<label for="add_form_title">Movie Title</label>
+				<input type="text" id="add_form_title" name="movieTitle" />
+				<input type="submit" />
+			</form>
+		"""
+		self.response.write(html_head + html_form + html_form)
 
-        # add Tomorrow's Movie to the response string
-        tomorrow_movie = self.getRandomMovie()
-        response += "<h1>Tomorrow's Movie</h1>"
-        response += "<p>" + tomorrow_movie + "</p>"
+class AddMovie(webapp2.RequestHandler):
+	### handle adding movies
+	###
 
-        self.response.write(response)
+	def post(self):
+		self.response.write(str(self.request))
+
 
 app = webapp2.WSGIApplication([
-    ('/', Index)
+    ('/', Index),
+    ('/addmovie', AddMovie)
 ], debug=True)
